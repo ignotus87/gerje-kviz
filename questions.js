11143fa2c79a8b3771e31b5f07401a6004b1ea84
Takacs, Zsolt <zsolt.takacs.ext@siemens-energy.com>
2024-04-24 17:58:17 +0200

    Separate questions from logic

---
+++ questions.js
@@ -0,0 +1,65 @@
+var HTMLquestions = [
+
+    {
+        question: "HTML is what type of language ?",
+        choices: ["Scripting Language", "Markup Language", "Programming Language", "Network Protocol"],
+        answer: 2
+
+    },
+
+    {
+        question: "HTML uses :",
+        choices: ["User defined tags", "Pre-specified tags", "Fixed tags defined by the language", "Tags only for linking"],
+        answer: 3
+
+    },
+    {
+        question: "The year in which HTML was first proposed _______.",
+        choices: ["1990", "1980", "2000", "1995"],
+        answer: 1
+
+    },
+    {
+        question: "Apart from <b> tag, what other tag makes text bold ?",
+        choices: ["fat", "strong", "black", "emp"],
+        answer: 2
+
+    },
+    {/*5*/
+        question: "How can you make a bulleted list with numbers? ",
+        choices: ["dl", "ol", "list", "ul"],
+        answer: 2
+
+    },
+    {
+        question: "What tag is used to display a picture in a HTML page?",
+        choices: ["picture", "image", "pic", "img"],
+        answer: 4
+
+    },
+    {
+        question: "HTML web pages can be read and rendered by _________.",
+        choices: ["Compiler", "Server", "Web Browser", "Interpreter"],
+        answer: 3
+
+    },
+    {
+        question: "Which of the following is not a browser ?",
+        choices: ["Microsofts Bing", "Netscape Navigator", "Mozilla Firefox", "Opera"],
+        answer: 1
+
+    },
+    {
+        question: "HTML tags are surrounded by which type of brackets.",
+        choices: ["Curly", "Round", "Squart", "Angle"],
+        answer: 4
+
+    },
+    {/*10*/
+        question: "Tags and test that are not directly displayed on the page are written in _____ section.",
+        choices: ["head", "title", "body", "html"],
+        answer: 1
+
+    }
+
+];
